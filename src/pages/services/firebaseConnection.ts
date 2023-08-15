0aa970e7d92718c21646a07035f578f3a782d301
Breno Bernardes <brenobernardes@Brenos-MacBook-Air.local>
2023-08-15 16:43:08 -0300

    minnor adjustments

--- src/pages/services/firebaseConnection.ts
+++ src/pages/services/firebaseConnection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: process.env.API_KEY as string,
+  authDomain: process.env.AUTH_DOMAIN as string,
+  projectId: process.env.PROJECT_ID as string,
+  storageBucket: process.env.STORAGE_BUCKET as string,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID as string,
+  appId: process.env.APP_ID as string
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
